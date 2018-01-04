483170f30a3abe6abf423521d2afb916841b6f31
Arya Bhushan <aryabhushan2003@gmail.com>
2018-01-04 10:28:53 +0530

    Add files via upload

---
+++ intelli.py
@@ -0,0 +1,21 @@
+print("WELCOME TO INTELLIGENCE LEVEL CHECKER")
+print("Test your intelligence level")
+name=input("\nWhat do I call you?\n")
+age=input("How old are you??\n")
+print("So, let\'s begin")
+print('''
+1. Play - If you have got guts.
+2. Rules - You gotta follow rules here...
+3. Terms & Conditions - In case you wanna read long, unnecessary stuff.
+''')
+general=input("Your selection: ")
+if general =="1":
+    print("Did you really thought this was a real game??")
+elif general =="2":
+    print("...nope, just kidding.")
+elif general =="3":
+    import this
+else:
+    print("Sorry, that was an improper selection.\n")
+    general=input("Your selection: ")
+exit("See ya next year {} ;)" .format(name))
---
+++ poem.py
@@ -0,0 +1,32 @@
+def poem(times):
+	print('''I didn’t know what went wrong, what made her walk away.
+I tried to figure out, but nothing she would say,
+For my questions pestered her, my tears made her embarrassed.
+I never wanted to be a priority, but a little love was all I expected.
+I still kept giving, giving, day in and out.
+But all went in vain, when all she beheld were doubts.
+Doubts of an uncertain tomorrow, so she told me, ‘I have to go.’
+My dreams, my wishes, my love were shattered in a single blow.
+I stood by the balcony, looking into the thin air.
+I clung on to her picture, crying silently so that no one saw.
+All I begged of my fate, was a sight of her.
+Occupied with things important, she had moved on well in life.
+It was never tough for her, for I was another man by her side.
+She thought I must be over her by then; least was she aware that I died each day.
+I moved out, I had forgotten how it felt being loved.
+I sought a place to pour my heart out, I looked below and above.
+When saw no place to confine, saw no place to cry my heart out,
+I kept walking towards a dead end, against the fate I had enough fought.
+I reached the valley, stood there high, saw down the abyss and looked behind.
+When saw no one who would call me back, I let my body be taken down by the wind.
+They located my body amidst the woods, with a note in my hand clutched tight.
+And a picture of her in my hand. It read, ‘I will love you till I die, don’t let her
+know of my death, She might live life in guilt.
+Just tell her I went to a place afar,
+And though I loved her still.’
+I stood there while they laid my corpse; a kid asked me who he was
+I told him ‘A part of me ’.\n'''*times)
+
+print('Here\'s a wonderful poem written by Arisch')
+times=int(input('How many times do you want poem to repeat?\n'))
+poem(times)
